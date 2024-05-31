4dc3d8ad7e66f66977601a62854b93459023b754
gwon.park <gwon.park@samsung.com>
2024-05-31 09:16:34 +0900

    Add Check twice length

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -3,6 +3,36 @@
 
 TEST(LengthTest, SameLength) {
 	Word word;
+	string target1{"abcd"};
+	string target2{"abcd"};
 	int res = 60;
-	EXPECT_EQ(60, word.CheckLength());
+
+	EXPECT_EQ(res, word.CheckLength(target1, target2));
+}
+
+TEST(LengthTest, TwiceLength1) {
+	Word word;
+	string target1{ "abcd" };
+	string target2{ "abcdefgh" };
+	int res = 0;
+
+	EXPECT_EQ(res, word.CheckLength(target1, target2));
+}
+
+TEST(LengthTest, TwiceLength2) {
+	Word word;
+	string target1{ "abcdefgh" };
+	string target2{ "abcd" };
+	int res = 0;
+
+	EXPECT_EQ(res, word.CheckLength(target1, target2));
+}
+
+TEST(LengthTest, TwiceLength3) {
+	Word word;
+	string target1{ "abcdefghsdf" };
+	string target2{ "abcd" };
+	int res = 0;
+
+	EXPECT_EQ(res, word.CheckLength(target1, target2));
 }
--- SimilarityChecker/word.cpp
+++ SimilarityChecker/word.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 class Word {
 public:
-	int CheckLength() {
-		return 60;
+	int CheckLength(string target1, string target2) {
+		if (target1.length() == target2.length())
+			return 60;
+		if (target1.length() > target2.length() &&
+			target1.length() >= target2.length() * 2) {
+			return 0;
+		}
+		if (target1.length() < target2.length() &&
+			target1.length() * 2 <= target2.length()) {
+			return 0;
+		}
 	}
 };
