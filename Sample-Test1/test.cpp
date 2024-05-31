9ac2877bf18c7b6e89330f945aead59719b635cc
gwon.park <gwon.park@samsung.com>
2024-05-31 10:05:42 +0900

    Add DifferentLength TC

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "../SimilarityChecker/word.cpp"
+#include <iostream>
 
 class WordFixture: public testing::Test {
 public:
@@ -8,28 +9,44 @@ public:
 
 TEST_F(WordFixture, SameLength) {
 	word.SetTarget("abcd", "abcd");
-	int res = 60;
+	double res = 60;
 
 	EXPECT_EQ(res, word.CheckLength());
 }
 
 TEST_F(WordFixture, TwiceLength1) {
 	word.SetTarget("abcd", "abcdefgh");
-	int res = 0;
+	double res = 0;
 
 	EXPECT_EQ(res, word.CheckLength());
 }
 
 TEST_F(WordFixture, TwiceLength2) {
 	word.SetTarget("abcdefgh", "abcd");
-	int res = 0;
+	double res = 0;
 
 	EXPECT_EQ(res, word.CheckLength());
 }
 
 TEST_F(WordFixture, TwiceLength3) {
 	word.SetTarget("abcdefghsdf", "abcd");
-	int res = 0;
+	double res = 0;
+
+	EXPECT_EQ(res, word.CheckLength());
+}
+
+TEST_F(WordFixture, DifferentLength1) {
+	word.SetTarget("abcdefg", "abcd");
+	double gap = double(7 - 4) / 4;
+	double res = (1 - gap) * 60;
+
+	EXPECT_EQ(res, word.CheckLength());
+}
+
+TEST_F(WordFixture, DifferentLength2) {
+	word.SetTarget("abcd", "abcdefg");
+	double gap = double(7 - 4) / 4;
+	double res = (1 - gap) * 60;
 
 	EXPECT_EQ(res, word.CheckLength());
 }
--- SimilarityChecker/word.cpp
+++ SimilarityChecker/word.cpp
@@ -4,21 +4,32 @@ using namespace std;
 
 class Word {
 public:
+	const int MAX_LENGTH_SCORE = 60;
+
 	void SetTarget(string target1, string target2) {
 		_target1 = target1;
 		_target2 = target2;
 	}
 
-	int CheckLength() {
-		if (_target1.length() == _target2.length())
+	double CheckLength() {
+		double length1 = _target1.length();
+		double length2 = _target2.length();
+
+		if (length1 == length2)
 			return 60;
-		if (_target1.length() > _target2.length() &&
-			_target1.length() >= _target2.length() * 2) {
+		if (length1 > length2 &&
+			length1 >= length2 * 2) {
 			return 0;
 		}
-		if (_target1.length() < _target2.length() &&
-			_target1.length() * 2 <= _target2.length()) {
+		if (length1 < length2 &&
+			length1 * 2 <= length2) {
 			return 0;
+		}
+		if (length1 > length2) {
+			return (1 - (length1 - length2) / length2) * MAX_LENGTH_SCORE;
+		}
+		if (length2 > length1) {
+			return (1 - (length2 - length1) / length1) * MAX_LENGTH_SCORE;
 		}
 	}
 
